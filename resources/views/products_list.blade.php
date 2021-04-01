8d3a521e246a4dd78c63f48be90afd3a12c6226c
Daniil <pde-rnd>
2021-04-01 18:57:10 +0300

    Применены стили к таблице

--- resources/views/products_list.blade.php
+++ resources/views/products_list.blade.php
@@ -1,11 +1,30 @@
+<style>
+    table, td, th {
+        border: 1px solid black;
+        border-collapse: collapse;
+        padding: 5px
+    }
+</style>
+
 <h1>Список товаров</h1>
 <a href="/products/add">Добавить товар</a>
 
 <table>
+    <thead>
+        <th>Номер</th>
+        <th>Название</th>
+        <th colspan="2">Действия</th>
+    </thead>
     @foreach ($products as $product)
     <tr>
         <td>{{ $product->id }}</td>
         <td>{{ $product->name }}</td>
+        <td><a href="/products/{{ $product->id }}">[e]</a></td>
+        <td>
+            <form action="/products/{{ $product->id }}" method="DELETE">
+                <button type="submit">x</button>
+            </form>
+        </td>
     </tr>
     @endforeach
 </table>
